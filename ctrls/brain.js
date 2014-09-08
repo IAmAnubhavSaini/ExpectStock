e677ccb62aa076bbe5f684cf2ae1e5482238fcb5
Bugeun Kim <cd4209@gmail.com>
2014-09-08 22:44:41 +0900

    fix brain : [0,1] to (-inf, inf)

--- ctrls/brain.js
+++ ctrls/brain.js
@@ -75,14 +75,14 @@ var trainNet = function ( code, callback ) {
         }
 
         var x = new brain.Vol(1, 1, 10);
-        x.w[0] = ma5 / (prev.start * 2);
-        x.w[1] = ma20 / (prev.start * 2);
-        x.w[2] = ma60 / (prev.start * 2);
-        x.w[4] = prev.close / (prev.start * 2);
-        x.w[5] = prev.high / (prev.start * 2);
-        x.w[6] = prev.low / (prev.start * 2);
-        x.w[7] = prev.NAV / (prev.start * 2);
-        x.w[8] = prev.volume / 10000000;
+        x.w[0] = ma5;
+        x.w[1] = ma20;
+        x.w[2] = ma60;
+        x.w[4] = prev.close;
+        x.w[5] = prev.high;
+        x.w[6] = prev.low;
+        x.w[7] = prev.NAV;
+        x.w[8] = prev.volume / 1000;
         x.w[9] = 1;
 
         var y = [];
@@ -130,20 +130,17 @@ module.exports = exports = {
         }
 
         var x = new brain.Vol(1, 1, 10);
-        x.w[0] = meanStock(data.slice(-5)) / (prev.start * 2);
-        x.w[1] = meanStock(data.slice(-20)) / (prev.start * 2);
-        x.w[2] = meanStock(data.slice(-60)) / (prev.start * 2);
-        x.w[4] = prev.close / (prev.start * 2);
-        x.w[5] = prev.high / (prev.start * 2);
-        x.w[6] = prev.low / (prev.start * 2);
-        x.w[7] = prev.NAV / (prev.start * 2);
-        x.w[8] = prev.volume / 10000000;
+        x.w[0] = meanStock(data.slice(-5));
+        x.w[1] = meanStock(data.slice(-20));
+        x.w[2] = meanStock(data.slice(-60));
+        x.w[4] = prev.close;
+        x.w[5] = prev.high;
+        x.w[6] = prev.low;
+        x.w[7] = prev.NAV;
+        x.w[8] = prev.volume / 1000;
         x.w[9] = 1;
 
         expect = exports.net[code].forward(x).w;
-        expect[0] = expect[0] * curr.start * 2;
-        expect[1] = expect[1] * curr.start * 2;
-        expect[2] = expect[2] * curr.start * 2;
       }
 
       callback({
