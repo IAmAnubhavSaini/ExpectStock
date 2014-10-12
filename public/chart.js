6429f42afc71ddd387a3ead92547f111ab41c2e3
nearbydelta <cd4209@gmail.com>
2014-10-13 01:02:39 +0900

    Chart!

--- public/chart.js
+++ public/chart.js
@@ -4,12 +4,12 @@ function loaded( dataset ){
     datasets : [
       {
         label : "Closing Price",
-        fillColor : "rgba(220,220,220,0.2)",
-        strokeColor : "rgba(220,220,220,1)",
-        pointColor : "rgba(220,220,220,1)",
-        pointStrokeColor : "#fff",
-        pointHighlightFill : "#fff",
-        pointHighlightStroke : "rgba(220,220,220,1)",
+        fillColor : "rgba(0,220,220,0.2)",
+        strokeColor : "rgba(0,220,220,1)",
+        pointColor : "rgba(0,220,220,1)",
+        pointStrokeColor : "#0ff",
+        pointHighlightFill : "#0ff",
+        pointHighlightStroke : "rgba(0,220,220,1)",
         data : dataset.stock
       }
     ]
@@ -19,12 +19,12 @@ function loaded( dataset ){
     datasets : [
       {
         label : "Closing Price",
-        fillColor : "rgba(220,220,220,0.2)",
-        strokeColor : "rgba(220,220,220,1)",
-        pointColor : "rgba(220,220,220,1)",
-        pointStrokeColor : "#fff",
-        pointHighlightFill : "#fff",
-        pointHighlightStroke : "rgba(220,220,220,1)",
+        fillColor : "rgba(220,0,0,0.2)",
+        strokeColor : "rgba(220,0,0,1)",
+        pointColor : "rgba(220,0,0,1)",
+        pointStrokeColor : "#f00",
+        pointHighlightFill : "#f00",
+        pointHighlightStroke : "rgba(220,0,0,1)",
         data : dataset.vol
       }
     ]
@@ -36,17 +36,19 @@ function loaded( dataset ){
   
   var stockctx = document.getElementById('stock-chart').getContext('2d');
   new Chart(stockctx).Line(stockData, {
+    showScale : false,
     scaleShowGridLines : false,
-    pointDot : false,
     scaleFontSize : 9,
     tooltipFontSize : 12,
     tooltipTitleFontSize : 12
   });
   var volctx = document.getElementById('vol-chart').getContext('2d');
   new Chart(volctx).Bar(volData, {
+    showScale : false,
     scaleShowGridLines : false,
     scaleFontSize : 9,
     tooltipFontSize : 12,
-    tooltipTitleFontSize : 12
+    tooltipTitleFontSize : 12,
+    barValueSpacing : 0
   });
 };
